1d17afd6ec8cbfc52874903d70142ad63c59be4e
Simon Thompson <simonthompson99@googlemail.com>
2022-04-08 12:53:01 +0100

    feat(eligible flag test added) new tests for eligible flag in patient_list
    Add in new tests for various different scenarios that will influence
    whether a patient is deemed eligible by vw_patient_list or not

--- test/data_for_testing.py
+++ test/data_for_testing.py
@@ -4,17 +4,29 @@ d = {
     'patient': {
         'uid': '92943acf-12f4-4c9b-8bb2-068b81a1d3b7',
         'patient_id': 'p001',
+        'patient_date_of_birth': '1950-01-01',
+        'life_status': 'alive',
     },
     'consent': {
         'consent_uid': 'be5e91a4-63de-49b4-b16d-d78368702369',
         'patient_uid': '92943acf-12f4-4c9b-8bb2-068b81a1d3b7',
+        'research_answer_given': 'yes',
+        'consent_category': 'Adult',
+        'consent_date': '2000-01-01',
         'recency': 1,
     },
-    'referral': {'uid': '362635c4-6227-479e-9197-3b7196317759', 'referral_id': 'r001'},
+    'referral': {
+        'uid': '362635c4-6227-479e-9197-3b7196317759',
+        'referral_id': 'r001',
+        'status': 'active',
+    },
     'referral_participant': {
         'uid': 'aa3097ec-ed5d-4785-b9f0-c6e689a5f609',
         'referral_id': 'r001',
         'patient_id': 'p001',
+    },
+    'closed_referral': {
+        'referral_id': 'r001',
     },
 }
 
---
+++ test/test_patient_eligibility_logic.py
@@ -0,0 +1,194 @@
+import subprocess
+import unittest
+from test import data_for_testing
+
+import data_transfer
+
+# establish all the difference scenarios we want to test, the sql we need to
+# run against the test data, and what the expected eligible would be
+test_scenarios = {
+    'no_change_to_test_participant': {
+        'sql': [],
+        'eligible': True,
+    },
+    'not_in_valid_referral': {
+        'sql': ["update referral set status = 'draft' where referral_id = 'r001';"],
+        'eligible': False,
+    },
+    'not_in_closed_case': {
+        'sql': ["delete from closed_referral where referral_id = 'r001';"],
+        'eligible': False,
+    },
+    'did_not_agree_to_research': {
+        'sql': [
+            (
+                "update consent set research_answer_given = 'no' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            )
+        ],
+        'eligible': False,
+    },
+    'deceased': {
+        'sql': [
+            "update patient set life_status = 'deceased' where patient_id = 'p001';"
+        ],
+        'eligible': True,
+    },
+    'on_child_consent_over_16_at_consent': {
+        'sql': [
+            (
+                "update consent set consent_category = 'Child' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set patient_date_of_birth = '2000-01-01' "
+                "where patient_id = 'p001';"
+            ),
+            (
+                "update consent set consent_date = '2016-01-02' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+        ],
+        'eligible': True,
+    },
+    'on_child_consent_under_16_at_consent_deceased': {
+        'sql': [
+            (
+                "update consent set consent_category = 'Child' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set patient_date_of_birth = '2000-01-01' "
+                "where patient_id = 'p001';"
+            ),
+            (
+                "update consent set consent_date = '2015-12-31' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            "update patient set life_status = 'deceased' where patient_id = 'p001';",
+        ],
+        'eligible': True,
+    },
+    'on_child_consent_over_16_at_consent_deceased': {
+        'sql': [
+            (
+                "update consent set consent_category = 'Child' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set patient_date_of_birth = '2000-01-01' "
+                "where patient_id = 'p001';"
+            ),
+            (
+                "update consent set consent_date = '2016-01-02' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set life_status = 'deceased' "
+                "where patient_id = 'p001';"
+            ),
+        ],
+        'eligible': True,
+    },
+    'on_child_consent_under_16_at_consent_under_16_at_release': {
+        'sql': [
+            (
+                "update consent set consent_category = 'Child' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set patient_date_of_birth = '2000-01-01' "
+                "where patient_id = 'p001';"
+            ),
+            "update release set release_date = '2015-12-31';",
+        ],
+        'eligible': True,
+    },
+    'on_child_consent_under_16_at_consent_over_16_at_release': {
+        'sql': [
+            (
+                "update consent set consent_category = 'Child' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set patient_date_of_birth = '2000-01-01' "
+                "where patient_id = 'p001';"
+            ),
+            "update release set release_date = '2016-01-02';",
+        ],
+        'eligible': False,
+    },
+    'on_child_consent_over_16_at_consent_over_16_at_release': {
+        'sql': [
+            (
+                "update consent set consent_category = 'Child' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            (
+                "update patient set patient_date_of_birth = '2000-01-01' "
+                "where patient_id = 'p001';"
+            ),
+            (
+                "update consent set consent_date = '2016-01-02' "
+                "where patient_uid = '92943acf-12f4-4c9b-8bb2-068b81a1d3b7';"
+            ),
+            "update release set release_date = '2016-01-02';",
+        ],
+        'eligible': True,
+    },
+}
+
+
+class TestPatientEligibilityLogic(unittest.TestCase):
+    def setUp(self):
+        """
+        build the intermediate database and load up the test patient data
+        """
+
+        subprocess.run(['make', 'build_dest_db'], capture_output=True)
+
+        d = data_for_testing.dict_to_dfs(data_for_testing.d)
+
+        with data_transfer.get_dest_connection() as con:
+
+            for k, v in d.items():
+
+                data_transfer.load_df_to_db(k, con, v)
+
+    def tearDown(self):
+        """
+        drop the intermediate database
+        """
+
+        subprocess.run(['make', 'drop_dest_db'], capture_output=True)
+
+
+def test_generator(sql_stmts, eligible):
+    """
+    generate a test function to attach to the unittest subclass
+    """
+
+    def test(self):
+
+        with data_transfer.get_dest_connection() as con:
+
+            for x in sql_stmts:
+
+                con.execute(x)
+
+            d = data_transfer.read_sql_to_df(
+                "select eligible from vw_patient_list where patient_id = 'p001';", con
+            )
+
+        self.assertEqual(eligible, d.eligible[0])
+
+    return test
+
+
+# go through and create test functions within unittest subclass for each
+# test scenario
+for ts, td in test_scenarios.items():
+
+    test = test_generator(td['sql'], td['eligible'])
+    test.__name__ = f'test_patient_eligibility_logic_{ts}'
+    setattr(TestPatientEligibilityLogic, test.__name__, test)
